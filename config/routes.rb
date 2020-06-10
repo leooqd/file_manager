9a296f56dbd01e4116b225080c3367f4bc19d219
Leonardo Quadros <leooq.d@gmail.com>
2020-06-09 22:20:47 -0300

    criando landing_page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	
+  get 'landing_page/home'
+  
+  root "landing_page#home"
+  get '*path', to: redirect('/'), constraints: lambda { |req|
+    req.path.exclude? 'rails/active_storage'
+  }
 end
